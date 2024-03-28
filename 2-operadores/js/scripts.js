837e150ea681b9b5e3f26fba52d8591db258b5e3
LARISSA TAMIRES DE ARAUJO NASCIMENTO <larissa.tanascimento@senacsp.edu.br>
2024-03-28 14:34:14 -0300

    introdução ao javascript

---
+++ 2-operadores/js/scripts.js
@@ -0,0 +1,70 @@
+console.log( typeof2)
+console.log(typeof2.2)
+console.log(typeof-4)
+console.log("Rodolfo")
+console.log(typeof"Rodolfo")
+
+
+// 2- operações aritimeticas
+
+console.log(2+4)
+console.log(10-5)
+console.log(5*4)
+console.log(10/2)
+
+//special numbers
+console.log(typeof Infinity)
+console.log(12*"asd")
+
+// 4- strings
+
+console.log( "um texto")
+console.log(`outro texto`)
+console.log(`mais um texto`)
+console.log(typeof "um texto")
+console.log(typeof "outro texto")
+console.log(typeof "mais um texto")
+
+//simbolos especiais em strings
+
+console.log("Testando|n quebra de linha")
+console.log("espaçamento |t de tab")
+
+//6-concatenação
+
+console.log("oi," + "tudo" + "bem?")
+
+console.log(`Testando uma concatenação` + "concatenação")
+console.log("")
+
+//7- Interpolação
+
+console.log(`A soma de 2+2é: ${2+2}`)
+console.log("A soma de 2+2é:" 2+2)
+
+
+console.log(`podemos executar qualquer coisa`)
+${console.log(`Teste 🧛‍♀️`)}
+
+//08-Booleans
+
+console.log(5>2)
+console.log(2>5)
+console.log(typeof(5>2))
+console.log(10==10)
+console.log(10==`10`)// 2 "=" verifica igualdade
+console.log(10===`10`)//3 "=" testa se é o mesmo numero e se é o mesmo tipo.
+console.log(10 !=9)// ! siginifica diferente
+console.log(10 !=`10`) // da true pq um é um numero e o outro uma string
+
+//11- op logico na pratica
+
+console.log(true && true)
+console.log(true &&false)
+
+console.log(5>2 && 2<10)
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
